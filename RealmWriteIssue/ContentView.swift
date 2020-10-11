5199ce1c3b2c2d49fa9a3be95b9dce2320a78f3f
Scott Carter <scarter28m-github@yahoo.com>
2020-10-11 10:19:17 -0400

    Initial Commit

---
+++ RealmWriteIssue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmWriteIssue
+//
+//  Created by Scott Carter on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealmWriteIssue/RealmWriteIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmWriteIssueApp.swift
+//  RealmWriteIssue
+//
+//  Created by Scott Carter on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RealmWriteIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
